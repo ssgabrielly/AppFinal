707aebca8076784e015845ae23f77a403c3f4d14
Gabrielly <ssgabi12@gmail.com>
2018-12-03 19:57:20 -0300

    App finalizado

--- app/src/main/java/com/mt/tecinfo/finalapplication/ActivityMedia.java
+++ app/src/main/java/com/mt/tecinfo/finalapplication/ActivityMedia.java
@@ -2,6 +2,7 @@ package com.mt.tecinfo.finalapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,6 +21,18 @@ public class ActivityMedia extends AppCompatActivity {
         final Button btnC = (Button) findViewById(R.id.btn_calc);
         final TextView dResult = (TextView) findViewById(R.id.displayResultado);
 
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double v1 = Double.parseDouble(nota1.getText().toString());
+                double v2 = Double.parseDouble(nota2.getText().toString());
+                double v3 = Double.parseDouble(nota3.getText().toString());
+                double v4 = Double.parseDouble(nota4.getText().toString());
 
+                double calc = (v1+v2+v3+v4)/4;
+                String media = Double.toString(calc);
+                dResult.setText(media);
+            }
+        });
     }
 }
